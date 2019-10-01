81f65d11f616444c49ca93185950918666a9a468
froggagul <froggagul@gmail.com>
2019-10-01 20:34:17 +0900

    removed frontend for last version

--- googlemap-next-reacthook-20190920/components/index.js
+++
@@ -1,3 +0,0 @@
-export {default as ContactInfo} from './ContactInfo';
-export {default as PlaceForm} from './PlaceForm';
-export {default as PlaceList} from './PlaceList';
--- googlemap-next-reacthook-20190920/dispatcher/index.js
+++
@@ -1,1 +0,0 @@
-export {default as PlaceListInfo} from './PlaceListInfo'; 
